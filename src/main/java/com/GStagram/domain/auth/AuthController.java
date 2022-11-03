fee2b50cc0c51453d75c2b78852142f42c43ad7c
Minhyeok1995 <76654360+minhyeok2487@users.noreply.github.com>
2022-11-03 15:33:17 +0900

    006. 회원가입 - 유효성검사와 MariaDB 연동

--- src/main/java/com/GStagram/domain/auth/AuthController.java
+++ src/main/java/com/GStagram/domain/auth/AuthController.java
@@ -1,9 +1,14 @@
 package com.GStagram.domain.auth;
 
 import com.GStagram.model.User;
+import java.util.HashMap;
+import java.util.Map;
+import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
@@ -25,12 +30,22 @@ public class AuthController {
 	}
 
 	@PostMapping("/auth/signup")
-	public String signup(SignupDto signupDto) { // key = value (x-www-form-urlencoded)
-		log.info(signupDto.toString());
-		User user = signupDto.toEntity();
-		log.info(user.toString());
-		User userEntity = authService.signupReg(user);
-		log.info(userEntity.toString());
+	public String signup(@Valid SignupDto signupDto, BindingResult bindingResult) { // key = value (x-www-form-urlencoded)
+		if (bindingResult.hasErrors()) {
+			Map<String, String> errorMap = new HashMap<>();
+			for (FieldError error : bindingResult.getFieldErrors()) {
+				errorMap.put(error.getField(), error.getDefaultMessage());
+				System.out.println("====================================");
+				System.out.println(error.getDefaultMessage());
+				System.out.println("====================================");
+			}
+		} else {
+			log.info(signupDto.toString());
+			User user = signupDto.toEntity();
+			log.info(user.toString());
+			User userEntity = authService.signupReg(user);
+			log.info(userEntity.toString());
+		}
 		return "auth/login"; // 회원가입이 완료되면 로그인 페이지로 이동한다.
 	}
 }
