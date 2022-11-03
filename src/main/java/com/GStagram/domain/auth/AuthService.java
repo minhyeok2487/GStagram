47d81e93822835878eea0c74a84987b74ae6daed
Minhyeok1995 <76654360+minhyeok2487@users.noreply.github.com>
2022-11-03 13:37:58 +0900

    005. 회원가입 - 비밀번호 암호화(Bcrypt)

--- src/main/java/com/GStagram/domain/auth/AuthService.java
+++ src/main/java/com/GStagram/domain/auth/AuthService.java
@@ -3,16 +3,23 @@ package com.GStagram.domain.auth;
 import com.GStagram.model.User;
 import com.GStagram.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @RequiredArgsConstructor
 @Service
 public class AuthService {
 
 	private final UserRepository userRepository;
+	private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
 	//회원 가입 진행
+	@Transactional
 	public User signupReg(User user) {
+		String encPassword = bCryptPasswordEncoder.encode(user.getPassword());
+		user.setPassword(encPassword);
+		user.setRole("ROLE_USER");
 		User userEntity = userRepository.save(user);
 		return userEntity;
 	}
