20c36199558fc7dd2be1e58ed54a6051d8258280
Minhyeok1995 <76654360+minhyeok2487@users.noreply.github.com>
2022-12-19 11:10:56 +0900

    회원가입 - 에러 Script 작성

--- src/main/java/com/GStagram/handler/ControllerExceptionHandler.java
+++ src/main/java/com/GStagram/handler/ControllerExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.GStagram.handler;
 
 import com.GStagram.handler.ex.CustomValidationException;
+import com.GStagram.util.Script;
 import com.GStagram.web.dto.CMRespDto;
 import java.util.Map;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -11,8 +12,17 @@ import org.springframework.web.bind.annotation.RestController;
 @ControllerAdvice
 public class ControllerExceptionHandler {
 
+//	@ExceptionHandler(CustomValidationException.class)
+//	public CMRespDto<?> validationException(CustomValidationException e) {
+//		return new CMRespDto(-1,e.getMessage(),e.getErrorMap());
+//	}
+
 	@ExceptionHandler(CustomValidationException.class)
-	public CMRespDto<?> validationException(CustomValidationException e) {
-		return new CMRespDto(-1,e.getMessage(),e.getErrorMap());
+	public String  validationException(CustomValidationException e) {
+		// CMRespDto, Script 비교
+		// 1. 클라이언트에게 응답할 때는 Script 좋음
+		// 2. Ajax통신 - CMRespDto
+		// 3. Android 통신 - CMRespDto
+		return Script.back(e.getErrorMap().toString());
 	}
 }
---
+++ src/main/java/com/GStagram/util/Script.java
@@ -0,0 +1,13 @@
+package com.GStagram.util;
+
+public class Script {
+
+	public static String back(String message) {
+		StringBuffer stringBuffer = new StringBuffer();
+		stringBuffer.append("<script>");
+		stringBuffer.append("alert('"+message+"');");
+		stringBuffer.append("history.back();");
+		stringBuffer.append("</script>");
+		return stringBuffer.toString();
+	}
+}
