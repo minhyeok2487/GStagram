32de6f72b9b178656b6ad5facebcc645d0170bd3
Minhyeok1995 <76654360+minhyeok2487@users.noreply.github.com>
2022-12-24 16:18:29 +0900

    로그인 기능 세션 정보 확인(@AuthenticationPrincipal)

--- src/main/java/com/GStagram/web/UserController.java
+++ src/main/java/com/GStagram/web/UserController.java
@@ -1,10 +1,14 @@
 package com.GStagram.web;
 
+import com.GStagram.config.auth.PrincipalDetails;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
+@Slf4j
 public class UserController {
 
 	@GetMapping("/user/{id}")
@@ -13,7 +17,8 @@ public class UserController {
 	}
 
 	@GetMapping("/user/{id}/update")
-	public String update(@PathVariable int id) {
+	public String update(@PathVariable int id, @AuthenticationPrincipal PrincipalDetails principalDetails) {
+		log.info(principalDetails.toString());
 		return "user/update";
 	}
 }
