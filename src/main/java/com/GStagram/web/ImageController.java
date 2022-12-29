a0db7c71502599bc7de01e8f92a1904b7c94385c
Minhyeok1995 <76654360+minhyeok2487@users.noreply.github.com>
2022-12-29 15:15:58 +0900

    회원정보 수정 기능 추가

--- src/main/java/com/GStagram/web/ImageController.java
+++ src/main/java/com/GStagram/web/ImageController.java
@@ -1,9 +1,14 @@
 package com.GStagram.web;
 
+import com.GStagram.domain.user.User;
+import javax.servlet.http.HttpSession;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
+@Slf4j
 public class ImageController {
 
 	@GetMapping({"/","/image/story"})
--- src/main/java/com/GStagram/web/UserController.java
+++ src/main/java/com/GStagram/web/UserController.java
@@ -1,9 +1,14 @@
 package com.GStagram.web;
 
 import com.GStagram.config.auth.PrincipalDetails;
+import com.GStagram.domain.user.User;
+import java.security.Principal;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
@@ -17,8 +22,10 @@ public class UserController {
 	}
 
 	@GetMapping("/user/{id}/update")
-	public String update(@PathVariable int id, @AuthenticationPrincipal PrincipalDetails principalDetails) {
+	public String update(@PathVariable int id, @AuthenticationPrincipal PrincipalDetails principalDetails, Model model) {
 		log.info(principalDetails.toString());
+		model.addAttribute("principalDetails", principalDetails.getUser());
 		return "user/update";
 	}
+
 }
